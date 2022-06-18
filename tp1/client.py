bc0e3201353157640915183ad844141c6afefbbd
Ábner Neves <abnermneves@gmail.com>
2022-06-18 20:13:31 -0300

    ipv4 funcionando. acrescentando compatibilidade com ipv6

--- tp1/client.py
+++ tp1/client.py
@@ -1,17 +1,31 @@
 import socket as skt
+import sys
+import pdb
 
-HOST = '127.0.0.1'
-PORT = 51511
+# definir como parametro na execução
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
+IPv = sys.argv[3]
+# HOSTv4 = '127.0.0.1'
+# HOSTv6 = '::1'
+# PORT = 51511
 BUFSZ = 512 # tamanho da mensagem a receber do servidor
 
-with skt.socket(skt.AF_INET, skt.SOCK_STREAM) as s:
-  s.connect((HOST, PORT))
-
-  msg = input()
-
-  while (msg != 'kill'):
-    b_msg = str.encode(msg)
-    s.sendall(b_msg)
-    data = s.recv(BUFSZ)
-    print(f'Received {data!r}')
+def main():
+  with skt.socket(skt.AF_INET if IPv == 'v4' else skt.AF_INET6, skt.SOCK_STREAM) as s:
+    s.connect((IP, PORT))
     msg = input()
+
+    while (msg != 'kill'):
+      b_msg = str.encode(msg)
+      s.sendall(b_msg)
+      response = s.recv(BUFSZ)
+      
+      if response.decode() == 'invalid command':
+        break
+
+      print(f'{response.decode()}')
+      msg = input()
+
+if __name__ == '__main__':
+  main()
--- tp1/server.py
+++ tp1/server.py
@@ -1,25 +1,163 @@
 import socket as skt
+import re
+import random
+import sys
+import pdb
 
-HOST = '127.0.0.1'
+# ------------- algumas funções úteis ------------- #
+def unpack_int(lista):
+  return ' '.join([f'{el:0>2}' for el in lista]) if lista else ''
+
+def unpack_float(lista):
+  return ' '.join([f'{el:.2f}' for el in lista]) if lista else ''
+
+def qtd_sensores():
+  qtd_sensores = 0
+  for _, sensores in equipamentos.items():
+    qtd_sensores += len(sensores)
+  return qtd_sensores
+
+def equipamento_invalido(id):
+  return id < 1 or id > 4
+
+def sensores_invalidos(sensores):
+  return any([s < 1 or s > 4 for s in sensores])
+
+
+# ------------- funções para a central ------------- #
+def adicionar_sensores(sensores, idEquipamento):
+  if equipamento_invalido(idEquipamento):
+    return 'invalid equipment'
+  if sensores_invalidos(sensores):
+    return 'invalid sensor'
+    
+  sensores_ja_existentes = [s for s in sensores if s in equipamentos[idEquipamento]]
+
+  # assumindo que precisa caber todos os sensores ou nenhum será adicionado
+  if qtd_sensores() + len(sensores) > MAX_SENSORS:
+    return 'limit exceeded'
+  if sensores_ja_existentes:
+    return f'sensor {unpack_int(sensores_ja_existentes)} already exists in {idEquipamento:0>2}'
+  
+  equipamentos[idEquipamento] += sensores
+
+  return f'sensor {unpack_int(sensores)} added'
+
+def remover_sensor(sensor, idEquipamento):
+  if equipamento_invalido(idEquipamento):
+    return 'invalid equipment'
+  if sensor < 1 or sensor > 4:
+    return 'invalid sensor'
+  if sensor not in equipamentos[idEquipamento]:
+    return f'sensor {sensor:0>2} does not exist in {idEquipamento:0>2}'
+
+  equipamentos[idEquipamento].remove(sensor)
+
+  return f'sensor {sensor:0>2} removed'
+
+def consultar_equipamento(idEquipamento):
+  if equipamento_invalido(idEquipamento):
+    return 'invalid equipment'
+  if not equipamentos[idEquipamento]:
+    return 'none'
+
+  return f'{unpack_int(equipamentos[idEquipamento])}'
+
+def consultar_variaveis(sensores, idEquipamento):
+  if equipamento_invalido(idEquipamento):
+    return 'invalid equipment'
+  if sensores_invalidos(sensores):
+    return 'invalid sensor'
+    
+  sensores_nao_instalados = [s for s in sensores if s not in equipamentos[idEquipamento]]
+
+  if sensores_nao_instalados:
+    return f'sensor(s) {unpack_int(sensores_nao_instalados)} not installed'
+
+  valores = [] 
+  for _ in range(len(sensores)):
+    valores.append(random.randint(0, 1000) / 100)
+
+  return unpack_float(valores)
+
+# ------------- constantes e afins ------------- #
+IPv = sys.argv[1]
+PORT = int(sys.argv[2])
+HOST = ''
+# HOSTv4 = '127.0.0.1'
+# HOSTv6 = '::1'
 PORT = 51511
 BUFSZ = 512 # tamanho da mensagem a receber do cliente
+MAX_SENSORS = 15
 
-# skt.AF_INET é para IPv4. ver como faz para aceitar também IPv6
-with skt.socket(skt.AF_INET, skt.SOCK_STREAM) as s:
-  s.bind((HOST, PORT)) # associa o socket com o endereço e a porta
-  s.listen() # espera a conexão de algum cliente
+# dicionário de pares equipamento : sensores
+equipamentos = {
+  1: [],
+  2: [],
+  3: [],
+  4: []
+}
 
-  # aceita uma conexão e retorna um objeto de conexão e um endereço
-  # conn é um objeto socket distinto do listening socket
-  conn, addr = s.accept()
+def main():
+  # if IPv == 'v4':
+  #   s = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
+  # else:
+  #   s = skt.socket(skt.AF_INET6, skt.SOCK_STREAM)
+  # ------------- conexão do socket ------------- #
+  with skt.socket(skt.AF_INET if IPv == 'v4' else skt.AF_INET6, skt.SOCK_STREAM) as s:
+    pdb.run('')
+    s.bind(('', PORT)) # associa o socket com todos os endereços e a porta
+    # s.bind((skt.))
+    s.listen() # espera a conexão de algum cliente
 
-  with conn:
-    print(f'Connected by {addr}')
-  
-    while True:
-      data = conn.recv(BUFSZ)
-      if not data:
-        break
-      conn.sendall(data + b' passadah')
+    # aceita uma conexão e retorna um objeto de conexão e um endereço
+    # conn é um objeto socket distinto do listening socket
+    conn, addr = s.accept()
 
-print(f'fechou')
+    with conn:
+      print(f'Connected by {addr}')
+    
+      while True:
+        data = conn.recv(BUFSZ)
+        if not data:
+          break
+
+        msg = data.decode()
+        response = ''
+
+        if re.fullmatch('add sensor (\d+ )+in \d+', msg):
+          ids = re.findall('\d+', msg)
+          ids = [int(id) for id in ids]
+          idEquipamento = ids[-1]
+          sensores = ids[:-1]
+          response = adicionar_sensores(sensores, idEquipamento)
+
+        elif re.fullmatch('remove sensor \d+ in \d+', msg):
+          ids = re.findall('\d+', msg)
+          ids = [int(id) for id in ids]
+          idEquipamento = ids[-1]
+          sensor = ids[0]
+          response = remover_sensor(sensor, idEquipamento)
+
+        elif re.fullmatch('list sensors in \d+', msg):
+          ids = re.findall('\d+', msg)
+          idEquipamento = int(ids[0])
+          response = consultar_equipamento(idEquipamento)
+
+        elif re.fullmatch('read (\d+ )+in \d+', msg):
+          ids = re.findall('\d+', msg)
+          ids = [int(id) for id in ids]
+          idEquipamento = ids[-1]
+          sensores = ids[:-1]
+          response = consultar_variaveis(sensores, idEquipamento)
+
+        else:
+          conn.sendall(str.encode('invalid command'))
+          conn.close()
+          break
+
+        print(equipamentos)
+        conn.sendall(str.encode(response))
+
+if __name__ == '__main__':
+  main()
