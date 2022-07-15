56cc277ab916c281d21addabf071407e0602ac3f
Ábner Neves <abnermneves@gmail.com>
2022-07-15 13:10:30 -0300

    código do servidor organizado e comentado

--- tp2/server.py
+++ tp2/server.py
@@ -1,13 +1,16 @@
-import socket as skt
 from _thread import *
+import socket as skt
+import pdb
 import sys
 import re
-import pdb
+
+# ------------------------------------ constantes e afins ------------------------------------ #
 
 PORT = int(sys.argv[1])
 BUFSZ = 512 # tamanho da mensagem a receber do cliente
 MAX_EQ = 15
 
+# IDs dos tipos de mensagens
 REQ_ADD = 1
 REQ_REM = 2
 RES_ADD = 3
@@ -17,6 +20,7 @@ RES_INF = 6
 ERROR = 7
 OK = 8
 
+# tabela de erros
 errors =  {
   1 : 'Equipment not found',
   2 : 'Source equipment not found',
@@ -24,31 +28,42 @@ errors =  {
   4 : 'Equipment limit exceeded'
 }
 
+# tabela de OKs
 success = {
   1 : 'Successful removal'
 }
 
+# lista e contador de equipamentos
 eq_count = 0
 equipments = {}
 
-def unpack_req_rem(msg):
-  # segundo id da mensagem
-  id = re.findall('\d+', msg)[1]
+# -------------------------- extração de informações das mensagens -------------------------- #
 
+# retorna o id do equipamento a ser removido
+def unpack_req_rem(msg):
+  id = re.findall('\d+', msg)[1]
   return int(id)
 
+# retorna id source e id target do request information
 def unpack_req_inf(msg):
-  # id source e id target
   ids = re.findall('\d+', msg)[1:3]
   return [int(id) for id in ids]
 
+
+# retorna id_source, id_target e o valor da informação solicitada
 def unpack_res_inf(msg):
-  # retorna id_source, id_target e info
+  # identifica números na mensagem
   numbers = msg.split(' ')
+
+  # remove o id da mensagem e retorna os outros
   values = [int(n) for n in numbers[1:3]]
   values.append(float(numbers[3]))
+
   return values
 
+# ------------------------------------ ações do servidor ------------------------------------ #
+
+# atende a solicitação de remoção de quipamento
 def remove_equipment(id):
   # se o equipamento não existe, retorna erro
   if id not in equipments:
@@ -59,46 +74,58 @@ def remove_equipment(id):
 
   return f'{OK} {success[1]}'
 
+# atende a solicitação de informação de um equipamento
 def request_information(msg):
   id_source, id_target = unpack_req_inf(msg)
 
+  # verificação de existência do equipamento de origem
   if id_source not in equipments:
     print('Equipment {id_source} not found')
     response = f'{ERROR} {errors[2]}'    
     return response, id_source
 
+  # verificação de existência do equipamento de destino
   if id_target not in equipments:
     print(f'Equipment {id_target} not found')
     response = f'{ERROR} {errors[3]}'
     return response, id_source
 
+  # se ambos existirem, repassa a mensagem para o equipamento de destino
   return msg, id_target
 
+# tratamento da informação solicitada
 def respond_information(msg):
   id_source, id_target, _ = unpack_res_inf(msg)
 
+  # verificação de existência do equipamento de origem
   if id_source not in equipments:
     print('Equipment {id_source} not found')
     response = f'{ERROR} {errors[2]}'    
     return response, id_source
 
+  # verificação de existência do equipamento de destino
   if id_target not in equipments:
     print(f'Equipment {id_target} not found')
     response = f'{ERROR} {errors[3]}'
     return response, id_target
 
+  # se ambos existirem, repassa a informação ao destino
   return msg, id_target 
 
+# -------------------------------------- funções úteis -------------------------------------- #
+
+# retorna o id da mensagem, sempre contido no início
 def get_id_msg(msg):
-  # retorna o id contido no início da mensagem
   id = re.findall('^\d+', msg)
   id = int(id[0]) if id else ''
 
   return id
 
+# retorna a lista de equipamentos como uma string
 def get_eq_list():
   return ' '.join([str(id) for id in equipments])
 
+# realiza transmissão em broadcast
 def broadcast(msg):
   for id, eq in equipments.items():
     try:
@@ -106,8 +133,13 @@ def broadcast(msg):
     except skt.error as e:
       print(f'broadcast error to eq {id}: {e}')
 
+# ---------------------------------- manipulação do cliente ---------------------------------- #
+
+# manipula cada cliente em threads múltiplas
 def client_handler(connection):
   global eq_count, errors, equipments, PORT, BUFSZ, MAX_EQ
+
+  # recusa a conexão caso o máximo de equipamentos tenha sido atingido
   if eq_count >= MAX_EQ:
     connection.sendall(str.encode(f'{ERROR} {errors[4]}'))
     connection.close()
@@ -119,48 +151,49 @@ def client_handler(connection):
   equipments[idEq] = connection
   print(f'Equipment {idEq} added')
 
-  # envia RES_ADD com id do equipamento
+  # broadcast de RES_ADD com id do novo equipamento
   broadcast(f'{RES_ADD} {idEq}')
 
-  # envia lista de equipamentos já conectados
+  # envia lista de equipamentos já conectados para o novo equipamento
   res_list = f'{RES_LIST} {get_eq_list()}'
   connection.sendall(str.encode(res_list))
 
+  # escuta as mensagens do equipamento
   while True:
     request = connection.recv(BUFSZ).decode('utf-8')
     id_msg = get_id_msg(request)
 
+    # identifica mensagem REQ_REM pelo seu id e realiza as ações
     if id_msg == REQ_REM:
       idEq = unpack_req_rem(request)
       response = remove_equipment(idEq)
+
       connection.sendall(str.encode(response))
       connection.close()
+
       print(f'Equipment {idEq} removed')
       broadcast(f'{REQ_REM} {idEq}')
-      break
 
+      break
+    
+    # identifica mensagem REQ_INF pelo seu id e realiza as ações
     elif id_msg == REQ_INF:
       response, send_to = request_information(request)
       equipments[send_to].sendall(str.encode(response))
 
+    # identifica mensagem RES_INF pelo seu id e realiza as ações
     elif id_msg == RES_INF:
       response, send_to = respond_information(request)
       equipments[send_to].sendall(str.encode(response))
 
-
-    reply = 'resposta: ' + request
-
-    if not request:
-      break
-
-    connection.sendall(str.encode(reply))
+    # if not request:
+    #   break
     
+  # connection.close()
 
-  connection.close()
+# ----------------------------------- execução do programa ----------------------------------- #
 
 def main():
-  thread_count = 0
-
   with skt.socket() as s:
     try:
       # associa o socket com todos os endereços disponíveis
@@ -170,13 +203,12 @@ def main():
 
     # espera a conexão de algum cliente
     s.listen()
-    print('Aguardando conexão...')
 
+    # aceita conexões e cria nova thread para cada uma
     while True:
       client, address = s.accept()
-      print('Conectado a: ' + address[0] + ' : ' + str(address[1]))
       start_new_thread(client_handler, (client, ))
-      thread_count += 1
+      # print('Conectado a: ' + address[0] + ' : ' + str(address[1]))
 
 if __name__ == '__main__':
   main()
